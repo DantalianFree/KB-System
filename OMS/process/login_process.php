c1f1690615107059d3805ca9050a9979cdcf46ee
DantalianFree <mdrojo.chmsu@gmail.com>
2024-12-07 20:09:22 +0800

    Update login_process.php

--- OMS/process/login_process.php
+++ OMS/process/login_process.php
@@ -6,24 +6,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $stmt = $conn->prepare("SELECT `Password` FROM Authentication WHERE Username = ?");
+    $stmt = $conn->prepare("SELECT `Password`, UserType FROM Authentication WHERE Username = ?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($storedHash);
+        $stmt->bind_result($storedHash, $usertype);
         $stmt->fetch();
 
         if (password_verify($password, $storedHash)) {
             $_SESSION['username'] = $username;
-            header("Location: ../index.php"); // Redirect to dashboard or homepage
+            $_SESSION['usertype'] = $usertype;
+
+            if($usertype == 'staff') {
+                header("Location: ../oms_dashboard.php");
+            } elseif($usertype == 'Manager') {
+                header("Location: ../../Inventory/src/dashboard.php");
+            } else {
+                header("Location: ../../Inventory/src/dashboard.php");
+            }
             exit;
         } else {
-            $_SESSION['error'] = "Invalid password."; // Store error in session
+            $_SESSION['error'] = 'Invalid password.';
         }
     } else {
-        $_SESSION['error'] = "Username not found."; // Store error in session
+        $_SESSION['error'] = 'Username not found.';
     }
 
     $stmt->close();
