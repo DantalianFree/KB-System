051fafc2959628dde5e59d5a0971dc0231040283
DantalianFree <mdrojo.chmsu@gmail.com>
2024-12-09 00:11:07 +0800

    Update conn.php

--- CRM/conn.php
+++ CRM/conn.php
@@ -1,5 +1,14 @@
 <?php
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'kb_crm';
 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if($conn->connect_error) {
+        die("Connection Failed: " . $conn->connect_error);
+    }
 
 
 ?>
